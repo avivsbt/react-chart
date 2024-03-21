bcdbbdaaa1a8391627226819bb556e957284b169
avivsbt <avivsbt@gmail.com>
2024-03-21 15:14:13 +0200

    [XAxis] added point

--- src/chart/components/XAxis/XAxis.tsx
+++ src/chart/components/XAxis/XAxis.tsx
@@ -1,15 +1,26 @@
 import { memo } from 'react';
 import './XAxis.css';
+import { chart } from '../../data';
 
 const XAxis: React.FC = () => {
   return (
-    <ul className="wrapper-xaxis">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
+    <>
+      <ul className="wrapper-xaxis">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <div className="wrapper-point">
+        {chart.map((item) => {
+          return <div className="point">*</div>
+        })}
+      </div>
+
+    </>
+
   );
 };
 
